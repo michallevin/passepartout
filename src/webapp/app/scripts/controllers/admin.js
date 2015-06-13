3ff975e66505bc175a690c3a57d7ee923c82b55e
Oded Golden <odedgolden@gmail.com>
2015-06-13 18:54:32 +0300

    admin stuff

---
+++ src/webapp/app/scripts/controllers/admin.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name passepartoutApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the passepartoutApp
+ */
+angular.module('passepartoutApp')
+.controller('AdminCrl', function ($scope, $http) {
+
+	$scope.isImporting = false;
+
+		
+	$scope.startImport = function() {
+		$AdminService.startImport(function() {
+			$scope.isImporting = true;	
+		});
+	};
+
+	$scope.cancelImport = function() {
+		$AdminService.cancelImport(function() {
+			$scope.isImporting = false;
+		});
+	};
+
+	$scope.getImportUpdate = function() {
+		$AdminService.getImportUpdate(function(isImporting) {
+			$scope.isImporting = isImporting;
+		});
+	};
+
+
+});
